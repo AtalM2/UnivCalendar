fd80667313fb06a4bf493d86b0ec122aed8d9e7c
Noémi SALAÜN <noemi.salaun@hotmail.fr>
2012-11-26 19:20:53 +0100

    API Google qui marche

--- src/calendar/UnivCalendar.java
+++ src/calendar/UnivCalendar.java
@@ -11,27 +11,29 @@ import java.net.MalformedURLException;
 public class UnivCalendar {
 
 	public static void main(String[] args) {
-		
+
 		// Récupération en local
 		try {
-			System.out.println(ICSFinder.getLocal("exemple.ics"));			
+			System.out.println(ICSFinder.getLocal("exemple.ics"));
 		} catch (FileNotFoundException e) {
-			System.out.println("FileNotFoundException : " + e.getLocalizedMessage());
+			System.out.println("FileNotFoundException : " + e.getMessage());
 		}
-		
+
 		// Récupération sur le serveur
 		try {
 			System.out.println(ICSFinder.getURL("http://www.edt-sciences.univ-nantes.fr/g78030.ics"));
 		} catch (MalformedURLException e) {
-			System.out.println("MalformedURLException : " + e.getLocalizedMessage());
+			System.out.println("MalformedURLException : " + e.getMessage());
 		} catch (IOException e) {
-			System.out.println("IOException : " + e.getLocalizedMessage());
+			System.out.println("IOException : " + e.getMessage());
 		}
-		
+
 		// Récupération google
 		// adresse : atal.univ.nantes@gmail.com
 		// passwd : jnatal44
-		GoogleCalendar google = new GoogleCalendar();
+		
+		//https://developers.google.com/google-apps/calendar/v2/developers_guide_java
+
 
 	}
 }
