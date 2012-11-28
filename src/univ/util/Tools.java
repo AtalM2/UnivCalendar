c11f9ce014b1cbdc7191ea6c36f2d802a0b241f3
Noémi SALAÜN <noemi.salaun@hotmail.fr>
2012-11-28 14:40:47 +0100

    Classe outil avec la fonction d'arrondi

---
+++ src/univ/util/Tools.java
@@ -0,0 +1,13 @@
+package univ.util;
+
+/**
+ *
+ * @author Noémi Salaün <noemi.salaun@etu.univ-nantes.fr>
+ */
+public class Tools {
+	
+	public static int floor(int number, int floor) {
+		return floor * ((number+floor/2)/floor);
+	}
+	
+}
