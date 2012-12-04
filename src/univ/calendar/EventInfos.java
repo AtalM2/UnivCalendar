2bc76acc26d4b09ac5dffaa18bd802d9a2f02efc
Noémi SALAÜN <noemi.salaun@gmail.com>
2012-12-04 18:18:52 +0100

    Modif des commentaires

--- src/univ/calendar/EventInfos.java
+++ src/univ/calendar/EventInfos.java
@@ -3,18 +3,17 @@ package univ.calendar;
 import java.awt.Color;
 
 /**
- * Classe représentant permettant d'ajouter des informations à un event. Ces
+ * Classe permettant d'ajouter des informations à un event. Ces
  * infos seront utilisées par l'IHM pour mettre en forme le calendrier
  *
- * @author
  * @authors Noémi Salaün, Joseph Lark
  */
 public class EventInfos {
 
-	private Color color;
+	private Color color; /** Couleur d'affichage de l'Event **/
 	private Event event;
-	private int column; // Index de la colonne d'affichage de l'Event
-	private int width; // Largeur de l'event, pour gérer les Events simultanés
+	private int column; /** Index de la colonne d'affichage de l'Event **/
+	private int width; /** Largeur de l'event, pour gérer les Events simultanés **/
 	
 	public EventInfos(Event e, int c, int w, Color co) {
 		event = e;
